510d238413ae54f303151316c155c07fe3edb3bb
TheDiamondPicks <thediamondpicks1@gmail.com>
2017-02-06 09:15:36 +1300

    Updated 2.0 and 2.1 - See Spigot for more information

--- src/main/java/tk/thediamondpicks/sc/Chat.java
+++ src/main/java/tk/thediamondpicks/sc/Chat.java
@@ -1,5 +1,6 @@
 package tk.thediamondpicks.sc;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.*;
 import java.util.*;
 import org.bukkit.plugin.*;
@@ -9,285 +10,173 @@ import org.bukkit.event.player.*;
 import org.bukkit.event.*;
 import org.bukkit.*;
 
-public class Chat extends JavaPlugin implements Listener {
-	private HashMap<String, Boolean> toggle;
+public class Chat extends JavaPlugin implements Listener{
 
-	public Chat() {
-		super();
-		this.toggle = new HashMap<String, Boolean>();
-	}
 
 	public void onEnable() {
 		this.saveDefaultConfig();
-		this.getServer().getPluginManager().registerEvents((Listener) this, (Plugin) this);
-		this.getLogger().info("Server Staff Chat has successfully started!");
+		this.getLogger().info("Ultra Simple Staff Chat has successfully started! Found " + getConfig().getConfigurationSection("chats").getKeys(false).size() + "chats");
+        getServer().getPluginManager().registerEvents(this, this);
+        loadConfigFile();
 
 	}
+    private void loadConfigFile() {
+        saveDefaultConfig();
+        if (getConfig().getConfigurationSection("formats") != null) {
+            getConfig().createSection("chats");
+            getConfig().set("chats.helper.format", getConfig().get("formats.helper"));
+            getConfig().set("chats.moderator.format", getConfig().get("formats.moderator"));
+            getConfig().set("chats.modplus.format", getConfig().get("formats.modplus"));
+            getConfig().set("chats.admin.format", getConfig().get("formats.admin"));
+            getConfig().set("chats.developer.format", getConfig().get("formats.developer"));
+            getConfig().set("chats.builder.format", getConfig().get("formats.builder"));
+            getConfig().set("chats.heads.format", getConfig().get("formats.heads"));
+            getConfig().set("chats.staff.format", getConfig().get("formats.staff"));
+            getConfig().createSection("options");
+            getConfig().set("options.allow-colour-codes", true);
+            getConfig().set("options.console-username", "&4CONSOLE");
+            getConfig().set("options.no-permission-message", "&cYou do not have permission to perform that command.");
+            getConfig().set("options.not-console-message", "&cConsole use only!");
+            getConfig().set("chats.helper.command", "hc");
+            getConfig().set("chats.helper.permission", "staffchat.helper");
+            getConfig().set("chats.moderator.command", "mc");
+            getConfig().set("chats.moderator.permission", "staffchat.mod");
+            getConfig().set("chats.modplus.command", "mpc");
+            getConfig().set("chats.modplus.permission", "staffchat.modplus");
+            getConfig().set("chats.admin.command", "ac");
+            getConfig().set("chats.admin.permission", "staffchat.admin");
+            getConfig().set("chats.developer.command", "dc");
+            getConfig().set("chats.developer.permission", "staffchat.dev");
+            getConfig().set("chats.builder.command", "bc");
+            getConfig().set("chats.builder.permission", "staffchat.builder");
+            getConfig().set("chats.heads.command", "hdc");
+            getConfig().set("chats.heads.permission", "staffchat.heads");
+            getConfig().set("chats.staff.command", "sc");
+            getConfig().set("chats.staff.permission", "staffchat.staff");
+            getConfig().set("formats", null);
+            saveConfig();
+        }
+
+    }
 
 	public boolean onCommand(final CommandSender s, final Command cmd, final String label, final String[] args) {
-		if (cmd.getName().equalsIgnoreCase("modchat") && s instanceof Player) {
-			final Player m = (Player) s;
-			if (m.hasPermission("staffchat.mod")) {
-				if (args.length == 0) {
-					m.sendMessage(ChatColor.RED + "Invalid Arguments! Use " + ChatColor.WHITE + "/mc <message>");
-				} else {
-					String ps = m.getName();
-					String msg = "";
+        ConfigurationSection k = getConfig().getConfigurationSection("chats");
+	    if (cmd.getName().equalsIgnoreCase("csc")) {
+            if (s instanceof ConsoleCommandSender) {
+                if (args.length == 0) {
+                    s.sendMessage(ChatColor.RED + "Incorrect Usage! Use" + ChatColor.WHITE + "/csc <chat command> <message>");
+                } else {
+                    for (String command : k.getKeys(false)) {
+                        String comd = getConfig().getString("chats." + command + ".command");
+                        if (args[0].equalsIgnoreCase(comd)) {
+                            String permission = getConfig().getString("chats." + command + ".permission");
+                            String format = getConfig().getString("chats." + command + ".format");
+                            String ps = getConfig().getString("options.console-username");
+                            String msg = "";
 
-					for (int i = 0; i < args.length; ++i) {
-						msg = String.valueOf(msg) + args[i] + ' ';
-					}
-					String mes = getConfig().getString("formats.moderator");
-					mes = mes.replace("%PLAYER%", ps);
-					mes = mes.replace("%MSG%", msg);
-					ChatColor.translateAlternateColorCodes('&', mes);
+                            for (int i = 1; i < args.length; ++i) {
+                                msg = String.valueOf(msg) + args[i] + ' ';
+                            }
+                            format = format.replace("%PLAYER%", ps);
+                            format = this.format(format);
+                            format = format.replace("%MSG%", msg);
+                            ChatColor.translateAlternateColorCodes('&', format);
+                            if (getConfig().getBoolean("options.allow-colour-codes")) {
+                                format = this.format(format);
+                            }
 
-					this.sendmods(mes);
-				}
-			}
-		}
-		if (cmd.getName().equalsIgnoreCase("adminchat") && s instanceof Player) {
-			final Player a = (Player) s;
-			if (a.hasPermission("staffchat.admin")) {
-				if (args.length == 0) {
-					a.sendMessage(ChatColor.RED + "Invalid Arguments! Use " + ChatColor.WHITE + "/ac <message>");
-				} else {
+                            for (Player player : this.getServer().getOnlinePlayers()) {
+                                if (player.hasPermission(permission)) {
+                                    player.sendMessage(format);
+                                }
+                            }
+                            getServer().getConsoleSender().sendMessage(format);
+                        }
+                    }
 
-					String msg = "";
+                }
+            } else if (s instanceof Player) {
+                String message = getConfig().getString("options.not-console-message");
+                message = format(message);
+                s.sendMessage(message);
+            }
+        }
 
-					for (int i = 0; i < args.length; ++i) {
-						msg = String.valueOf(msg) + args[i] + ' ';
-					}
-					String ps = a.getName();
-					String mes = getConfig().getString("formats.admin");
-					mes = mes.replace("%PLAYER%", ps);
-					mes = mes.replace("%MSG%", msg);
-					ChatColor.translateAlternateColorCodes('&', mes);
+            if (cmd.getName().equalsIgnoreCase("screload")) {
+                if (s.hasPermission("staffchat.reload")) {
+                    this.reloadConfig();
+                    s.sendMessage(ChatColor.DARK_AQUA + "[Ultra Simple Staff Chat] " + ChatColor.AQUA + "The configuration was reloaded! Found " + ChatColor.WHITE + getConfig().getConfigurationSection("chats").getKeys(false).size() + ChatColor.AQUA+ " chats");
+                }
+                else {
+                    String message = getConfig().getString("options.no-permission-message");
+                    message = format(message);
+                    s.sendMessage(message);
+                }
+            }
+            return true;
+        }
 
-					this.sendadmins(mes);
-				}
-			}
-		}
-		if (cmd.getName().equalsIgnoreCase("helperchat") && s instanceof Player) {
-			final Player h = (Player) s;
-			if (h.hasPermission("staffchat.helper")) {
-				if (args.length == 0) {
-					h.sendMessage(ChatColor.RED + "Invalid Arguments! Use " + ChatColor.WHITE + "/hc <message>");
-				} else {
 
-					String msg = "";
+    @EventHandler
+    public void onPreprocess(PlayerCommandPreprocessEvent e) {
+        ConfigurationSection s = getConfig().getConfigurationSection("chats");
 
-					for (int i = 0; i < args.length; ++i) {
-						msg = String.valueOf(msg) + args[i] + ' ';
-					}
-					String ps = h.getName();
-					String mes = getConfig().getString("formats.helper");
-					mes = mes.replace("%PLAYER%", ps);
-					mes = mes.replace("%MSG%", msg);
-					ChatColor.translateAlternateColorCodes('&', mes);
+        for (String command : s.getKeys(false)) {
+            String cmd = getConfig().getString("chats." + command + ".command");
+            String[] args = e.getMessage().split(" ");
+            if (args[0].equalsIgnoreCase("/" + cmd)) {
+                String permission = getConfig().getString("chats." + command + ".permission");
+                String format = getConfig().getString("chats." + command + ".format");
 
-					this.sendhelpers(mes);
-				}
-			}
-		}
-		if (cmd.getName().equalsIgnoreCase("modpluschat") && s instanceof Player) {
-			final Player a = (Player) s;
-			if (a.hasPermission("staffchat.modplus")) {
-				if (args.length == 0) {
-					a.sendMessage(ChatColor.RED + "Invalid Arguments! Use " + ChatColor.WHITE + "/mpc <message>");
-				} else {
 
-					String msg = "";
 
-					for (int i = 0; i < args.length; ++i) {
-						msg = String.valueOf(msg) + args[i] + ' ';
-					}
-					String ps = a.getName();
-					String mes = getConfig().getString("formats.modplus");
-					mes = mes.replace("%PLAYER%", ps);
-					mes = mes.replace("%MSG%", msg);
-					ChatColor.translateAlternateColorCodes('&', mes);
+                Player m = e.getPlayer();
+                if (m.hasPermission(permission)) {
+                    if (args.length == 1) {
+                        m.sendMessage(ChatColor.RED + "Invalid Arguments! Use " + ChatColor.WHITE + "/" + cmd + " <message>");
+                    } else {
+                        String ps = m.getName();
+                        String msg = "";
 
-					this.sendmodplus(mes);
-				}
-			}
-		}
-		if (cmd.getName().equalsIgnoreCase("devchat") && s instanceof Player) {
-			final Player a = (Player) s;
-			if (a.hasPermission("staffchat.dev")) {
-				if (args.length == 0) {
-					a.sendMessage(ChatColor.RED + "Invalid Arguments! Use " + ChatColor.WHITE + "/dc <message>");
-				} else {
+                        for (int i = 1; i < args.length; ++i) {
+                            msg = String.valueOf(msg) + args[i] + ' ';
+                        }
+                        format = format.replace("%PLAYER%", ps);
+                        format = this.format(format);
+                        format = format.replace("%MSG%", msg);
+                        ChatColor.translateAlternateColorCodes('&', format);
+                        if (getConfig().getBoolean("options.allow-colour-codes")) {
+                            format = this.format(format);
+                        }
 
-					String msg = "";
+                        for (Player player : this.getServer().getOnlinePlayers()) {
+                            if (player.hasPermission(permission)) {
+                                player.sendMessage(format);
+                            }
+                        }
+                        getServer().getConsoleSender().sendMessage(format);
+                    }
 
-					for (int i = 0; i < args.length; ++i) {
-						msg = String.valueOf(msg) + args[i] + ' ';
-					}
-					String ps = a.getName();
-					String mes = getConfig().getString("formats.developer");
-					mes = mes.replace("%PLAYER%", ps);
-					mes = mes.replace("%MSG%", msg);
-					ChatColor.translateAlternateColorCodes('&', mes);
 
-					this.senddevs(mes);
-				}
-			}
-		}
-		if (cmd.getName().equalsIgnoreCase("headchat") && s instanceof Player) {
-			final Player a = (Player) s;
-			if (a.hasPermission("staffchat.heads")) {
-				if (args.length == 0) {
-					a.sendMessage(ChatColor.RED + "Invalid Arguments! Use " + ChatColor.WHITE + "/hdc <message>");
-				} else {
+                }
+                else {
+                    String message = getConfig().getString("options.no-permission-message");
+                    message = format(message);
+                    e.getPlayer().sendMessage(message);
+                }
+                e.setCancelled(true);
+            }
 
-					String msg = "";
+            //if (!s.getKeys(false).contains(e.getMessage().replace("/", ""))) {
+            //    e.setCancelled(false);
+            //}
+        }
+    }
 
-					for (int i = 0; i < args.length; ++i) {
-						msg = String.valueOf(msg) + args[i] + ' ';
-					}
-					String ps = a.getName();
-					String mes = getConfig().getString("formats.heads");
-					mes = mes.replace("%PLAYER%", ps);
-					mes = mes.replace("%MSG%", msg);
-					ChatColor.translateAlternateColorCodes('&', mes);
 
-					this.sendheads(mes);
-				}
-			}
-		}
-		if (cmd.getName().equalsIgnoreCase("builderchat") && s instanceof Player) {
-			final Player a = (Player) s;
-			if (a.hasPermission("staffchat.builder")) {
-				if (args.length == 0) {
-					a.sendMessage(ChatColor.RED + "Invalid Arguments! Use " + ChatColor.WHITE + "/bc <message>");
-				} else {
+    private String format ( final String input){
+        return ChatColor.translateAlternateColorCodes('&', input);
+    }
 
-					String msg = "";
 
-					for (int i = 0; i < args.length; ++i) {
-						msg = String.valueOf(msg) + args[i] + ' ';
-					}
-					String ps = a.getName();
-					String mes = getConfig().getString("formats.builder");
-					mes = mes.replace("%PLAYER%", ps);
-					mes = mes.replace("%MSG%", msg);
-					ChatColor.translateAlternateColorCodes('&', mes);
-
-					this.sendbuilders(mes);
-				}
-			}
-		}
-		if (cmd.getName().equalsIgnoreCase("staffchat") && s instanceof Player) {
-			final Player a = (Player) s;
-			if (a.hasPermission("staffchat.staff")) {
-				if (args.length == 0) {
-					a.sendMessage(ChatColor.RED + "Invalid Arguments! Use " + ChatColor.WHITE + "/sc <message>");
-				} else {
-
-					String msg = "";
-
-					for (int i = 0; i < args.length; ++i) {
-						msg = String.valueOf(msg) + args[i] + ' ';
-					}
-					String ps = a.getName();
-					String mes = getConfig().getString("formats.staff");
-					mes = mes.replace("%PLAYER%", ps);
-					mes = mes.replace("%MSG%", msg);
-					ChatColor.translateAlternateColorCodes('&', mes);
-
-					this.sendallstaff(mes);
-				}
-			}
-		}
-		if (cmd.getName().equalsIgnoreCase("screload") && s instanceof Player) {
-			Player a = (Player) s;
-			if (a.hasPermission("staffchat.reload")) {
-				this.reloadConfig();
-				a.sendMessage(ChatColor.DARK_AQUA + "[Staff Chat] " + ChatColor.AQUA + "The configuration was reloaded!");
-			}
-		}
-		return true;
-	}
-
-	@EventHandler
-	public void onPlayerJoin(final PlayerJoinEvent e) {
-		this.toggle.put(e.getPlayer().getName(), false);
-	}
-
-	private void sendmods(String msg) {
-		msg = this.format(msg);
-		for (Player player : this.getServer().getOnlinePlayers()) {
-			if (player.hasPermission("staffchat.mod")) {
-				player.sendMessage(msg);
-			}
-		}
-	}
-
-	private void sendadmins(String msg) {
-		msg = this.format(msg);
-		for (Player player : this.getServer().getOnlinePlayers()) {
-			if (player.hasPermission("staffchat.admin")) {
-				player.sendMessage(msg);
-			}
-		}
-	}
-
-	private void sendhelpers(String msg) {
-		msg = this.format(msg);
-		for (Player player : this.getServer().getOnlinePlayers()) {
-			if (player.hasPermission("staffchat.helper")) {
-				player.sendMessage(msg);
-			}
-		}
-	}
-
-	private void sendmodplus(String msg) {
-		msg = this.format(msg);
-		for (Player player : this.getServer().getOnlinePlayers()) {
-			if (player.hasPermission("staffchat.modplus")) {
-				player.sendMessage(msg);
-			}
-		}
-	}
-
-	private void senddevs(String msg) {
-		msg = this.format(msg);
-		for (Player player : this.getServer().getOnlinePlayers()) {
-			if (player.hasPermission("staffchat.dev")) {
-				player.sendMessage(msg);
-			}
-		}
-	}
-
-	private void sendbuilders(String msg) {
-		msg = this.format(msg);
-		for (Player player : this.getServer().getOnlinePlayers()) {
-			if (player.hasPermission("staffchat.builder")) {
-				player.sendMessage(msg);
-			}
-		}
-	}
-
-	private void sendheads(String msg) {
-		msg = this.format(msg);
-		for (Player player : this.getServer().getOnlinePlayers()) {
-			if (player.hasPermission("staffchat.heads")) {
-				player.sendMessage(msg);
-			}
-		}
-	}
-
-	private void sendallstaff(String msg) {
-		msg = this.format(msg);
-		for (Player player : this.getServer().getOnlinePlayers()) {
-			if (player.hasPermission("staffchat.staff")) {
-				player.sendMessage(msg);
-			}
-		}
-	}
-
-	private String format(final String input) {
-		return ChatColor.translateAlternateColorCodes('&', input);
-	}
 }
